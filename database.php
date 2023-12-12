90a46e449ad9b20623649b5957f7fe18499f86c0
Salma-Akter-B00942721 <Akter-S13@ulster.ac.uk>
2023-12-12 21:23:58 +0000

    first commit

--- database.php
+++ database.php
@@ -1,10 +1,15 @@
 <?php
-
-$host = 'salma123.database.windows.net.mysql.database.azure.com';
-$username = 'adminsalma@yourserver';
+$host = 'salma123.mysql.database.azure.com';
+$username = 'adminsalma@salma123';
 $password = 'Seuh4khbp16';
 $database = 'videodb';
 $port = 3306;
-$con = "mysql:host=$host;port=$port;dbname=$database;charset=utf8mb4";
 
+try {
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$database;charset=utf8mb4", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
